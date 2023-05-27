8f677d060ab78751a766d5bc8add1afc24c5f271
Ashsoaress <130417158+Ashsoaress@users.noreply.github.com>
2023-05-27 00:43:14 -0400

    Update server.js

--- movie_api/server.js
+++ movie_api/server.js
@@ -1,11 +1,4 @@
-const http = require('http');
 
-http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello Node!\n');
-}).listen(8080);
-
-console.log('My first Node test server is running on Port 8080.');
 
 if (q.pathname.includes('documentation')) {
   filePath = (__dirname + '/documentation.html');
